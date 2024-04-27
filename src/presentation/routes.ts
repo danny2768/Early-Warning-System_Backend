6e7197a454216b42847a20567744c0a76e38876c
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-04-27 11:27:38 -0500

    27/04/2024: Fix: readings endpoint

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,6 +3,7 @@ import { AuthRoutes } from "./auth/routes";
 import { SensorsRoutes } from "./sensors/routes";
 import { StationsRoutes } from "./stations/routes";
 import { NetworksRoutes } from "./networks/routes";
+import { ReadingsRoutes } from "./readings/routes";
 
 export class AppRoutes {
 
@@ -10,9 +11,8 @@ export class AppRoutes {
         const router = Router();
 
         // Routes
-        router.use('/api/auth', AuthRoutes.routes );    
-        // TODO: Add routes for readings
-        // router.use('/api/readings', /*SensorsRoutes.routes*/ );
+        router.use('/api/auth', AuthRoutes.routes );            
+        router.use('/api/readings', ReadingsRoutes.routes );
         router.use('/api/sensors', SensorsRoutes.routes );
         router.use('/api/stations', StationsRoutes.routes );
         router.use('/api/networks', NetworksRoutes.routes );        
