c97da1a4fec750ea8f00d724b59e8ff162e2aa00
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-05-16 23:08:31 -0500

    16/05/2024: Readings swagger documentation

--- src/presentation/readings/routes.ts
+++ src/presentation/readings/routes.ts
@@ -24,3 +24,103 @@ export class ReadingsRoutes {
     };
 }
 
+/**
+ * @swagger
+ * tags:
+ *   name: Readings
+ *   description: Readings resource
+ */
+
+/**
+ * @swagger
+ * /api/readings:
+ *  get:
+ *      summary: Get all readings
+ *      tags: [Readings]
+ *      responses:
+ *          200:
+ *              description: OK
+ */
+
+/**
+ * @swagger
+ * /api/readings:
+ *   post:
+ *     summary: Create a new reading
+ *     tags: [Readings]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               value:
+ *                 type: string
+ *               sensor:
+ *                 type: string
+ *               sentAt:
+ *                 type: string
+ *     responses:
+ *       "200":
+ *         description: A reading schema
+ *       "400":
+ *         description: Bad request
+ */
+
+/**
+ * @swagger
+ * /api/readings/{id}:
+ *   put:
+ *     summary: Update a reading by id
+ *     tags: [Readings]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Reading id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               value:
+ *                 type: string
+ *               sensor:
+ *                 type: string
+ *               sentAt:
+ *                 type: string
+ *     responses:
+ *       "200":
+ *         description: A reading schema
+ *       "400":
+ *         description: Bad request
+ *       "404":
+ *         description: Reading not found
+ */
+
+/**
+ * @swagger
+ * /api/readings/{id}:
+ *   delete:
+ *     summary: Delete a reading by id
+ *     tags: [Readings]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: Reading deleted successfully
+ *       "400":
+ *         description: Bad request
+ *       "404":
+ *         description: Reading not found
+ */
