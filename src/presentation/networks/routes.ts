e07eea98956d207a537fcce22557829c93507136
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-05-17 00:16:12 -0500

    17/05/2024: Networks swagger documentation

--- src/presentation/networks/routes.ts
+++ src/presentation/networks/routes.ts
@@ -22,3 +22,118 @@ export class NetworksRoutes {
         return router;
     };
 }
+
+/**
+ * @swagger
+ * tags:
+ *   name: Networks
+ *   description: Networks resource
+ */
+
+/**
+ * @swagger
+ * /api/networks:
+ *  get:
+ *      summary: Get all networks
+ *      tags: [Networks]
+ *      responses:
+ *          200:
+ *              description: OK
+ */
+
+/**
+ * @swagger
+ * /api/networks/{id}:
+ *  get:
+ *      summary: Get network by id
+ *      tags: [Networks]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: network id
+ *      responses:
+ *          200:
+ *              description: OK
+ */
+
+/**
+ * @swagger
+ * /api/networks:
+ *   post:
+ *     summary: Create a new network
+ *     tags: [Networks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       "200":
+ *         description: A network schema
+ *       "400":
+ *         description: Bad request
+ */
+
+/**
+ * @swagger
+ * /api/networks/{id}:
+ *   put:
+ *     summary: Update a network by id
+ *     tags: [Networks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: network id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       "200":
+ *         description: Network updated successfully
+ *       "400":
+ *         description: Bad request
+ *       "404":
+ *         description: Network not found
+ */
+
+/**
+ * @swagger
+ * /api/networks/{id}:
+ *   delete:
+ *     summary: Delete a network by id
+ *     tags: [Networks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Network id
+ *     responses:
+ *       "200":
+ *         description: Network deleted successfully
+ *       "400":
+ *         description: Bad request
+ *       "404":
+ *         description: Network not found
+ */
