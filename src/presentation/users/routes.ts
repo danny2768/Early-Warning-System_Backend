2a67cc1f404ed3fc6044bca581c2383151ffabdd
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-05-16 22:52:03 -0500

    16/05/2024: Users swagger documentation

--- src/presentation/users/routes.ts
+++ src/presentation/users/routes.ts
@@ -33,3 +33,128 @@ export class UsersRoutes {
         return router;
     }
 }
+
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: Users resource
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *  get:
+ *      summary: Get all users
+ *      tags: [Users]
+ *      responses:
+ *          200:
+ *              description: OK
+ */
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *  get:
+ *      summary: Get a user by id
+ *      tags: [Users]
+ *      parameters:
+ *          - in: path
+ *            name: id
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: User id
+ *      responses:
+ *          200:
+ *              description: OK
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *                 enum: ['USER_ROLE', 'ADMIN_ROLE']
+ *     responses:
+ *       "200":
+ *         description: A user schema
+ *       "400":
+ *         description: Bad request
+ */
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Update a user by id
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: User id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *                 enum: ['USER_ROLE', 'ADMIN_ROLE']
+ *     responses:
+ *       "200":
+ *         description: User updated successfully
+ *       "400":
+ *         description: Bad request
+ *       "404":
+ *         description: User not found
+ */
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Delete a user by id
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: User deleted successfully
+ *       "400":
+ *         description: Bad request
+ *       "404":
+ *         description: User not found
+ */
