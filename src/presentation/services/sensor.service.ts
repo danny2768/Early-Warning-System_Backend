ef6d2d571d272152c4d8d8cfecd8f37003b3132f
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-06-17 20:48:01 -0500

    17/06/2024: Improving error handling

--- src/presentation/services/sensor.service.ts
+++ src/presentation/services/sensor.service.ts
@@ -43,24 +43,31 @@ export class SensorService {
             }
 
         } catch (error) {
+            if (error instanceof CustomError) throw error;
             throw CustomError.internalServer(`${error}`);            
         }        
     };
 
     public async getSensorById( id: string ) {
-        this.sharedService.validateId(id);
-        const sensor = await SensorModel.findById(id);
-        if (!sensor) throw CustomError.badRequest(`No sensor with id ${id} has been found`);
+        try {
+            this.sharedService.validateId(id);
+            const sensor = await SensorModel.findById(id);
+            if (!sensor) throw CustomError.badRequest(`No sensor with id ${id} has been found`);
 
-        return SensorEntity.fromObj(sensor);
+            return SensorEntity.fromObj(sensor);   
+        } catch (error) {
+            if (error instanceof CustomError) throw error;
+            throw CustomError.internalServer(`${error}`);
+        }                
     };
 
     public async getSensorsByStationId( stationId: string ) {
-        this.validateStationId(stationId);
         try {
+            await this.validateStationId(stationId);
             const sensors = await SensorModel.find({ stationId });
             return sensors.map(sensor => SensorEntity.fromObj(sensor));            
         } catch (error) {
+            if (error instanceof CustomError) throw error;
             throw CustomError.internalServer(`${error}`);
         }        
     };
@@ -95,32 +102,34 @@ export class SensorService {
                 readings: readingsObj
             }
         } catch (error) {
+            if (error instanceof CustomError) throw error;
             throw CustomError.internalServer(`${error}`);            
         }        
     }
 
-    public async createSensor( createSensorDto: CreateSensorDto ) {
-        const existsSensor = await SensorModel.findOne({ name: createSensorDto.name });
-        if (existsSensor) throw CustomError.badRequest('Sensor already exists');
-                
-        await this.validateStationId(createSensorDto.stationId);
-
+    public async createSensor( createSensorDto: CreateSensorDto ) {        
         try {            
+            const existsSensor = await SensorModel.findOne({ name: createSensorDto.name });
+            if (existsSensor) throw CustomError.badRequest('Sensor already exists');
+                    
+            await this.validateStationId(createSensorDto.stationId);
+
             const sensor = new SensorModel(createSensorDto);
             await sensor.save();
             
             return SensorEntity.fromObj(sensor);
         } catch (error) {
+            if (error instanceof CustomError) throw error;
             throw CustomError.internalServer(`${error}`);
         }
     };
 
     public async updateSensor( updateSensorDto: UpdateSensorDto ) {
         const { id, ...updateOptions } = updateSensorDto;
-        this.sharedService.validateId(id);
-
-        if ( updateOptions.stationId ) await this.validateStationId(updateOptions.stationId);
         try {
+            this.sharedService.validateId(id);    
+            if ( updateOptions.stationId ) await this.validateStationId(updateOptions.stationId);
+
             const sensor = await SensorModel.findByIdAndUpdate(id, updateOptions, { new: true });
             if (!sensor) throw CustomError.badRequest(`No sensor with id ${id} has been found`);
             return SensorEntity.fromObj(sensor);
@@ -130,8 +139,9 @@ export class SensorService {
     };
 
     public async deleteSensor( id: string ) {
-        this.sharedService.validateId(id);
         try {
+            this.sharedService.validateId(id);
+            
             const sensor = await SensorModel.findByIdAndDelete(id);
             if (!sensor) throw CustomError.badRequest(`No sensor with id ${id} has been found`);
             return {
@@ -139,6 +149,7 @@ export class SensorService {
                 sensor: SensorEntity.fromObj(sensor)
             }
         } catch (error) {
+            if (error instanceof CustomError) throw error;
             throw CustomError.internalServer(`${error}`);            
         }        
     };
