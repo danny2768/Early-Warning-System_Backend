92d30964dfc8ad38f5d71084c6846dcac55b4b71
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-06-23 15:30:43 -0500

    23/06/2024: Adding method validateSelforAdminToken

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -32,6 +32,34 @@ export class AuthMiddleware {
         }
     }
 
+    static async validateSelfOrAdminToken(req: Request, res: Response, next: NextFunction) {
+        const authorization = req.header('Authorization');
+        if (!authorization) return res.status(401).json({ error: 'Unauthorized. No token provided' });
+
+        if (!authorization.startsWith('Bearer ')) return res.status(401).json({ error: 'Unauthorized. Invalid Bearer token' });
+
+        const token = authorization.split(' ').at(1) || '';
+
+        try {
+            const payload = await JwtAdapter.verifyToken<{ id: string, role: string }>(token);
+            if (!payload) return res.status(401).json({ error: 'Unauthorized. Invalid token' });
+
+            const user = await UserModel.findById(payload.id);
+            if (!user) return res.status(401).json({ error: 'Unauthorized. Invalid token' });
+
+            // Check if the user is updating their own information or is an admin
+            if (req.params.id === payload.id || user.role.includes('ADMIN_ROLE')) {
+                req.body.user = UserEntity.fromObj(user);
+                next();
+            } else {
+                return res.status(403).json({ error: 'Forbidden. You dont have permission to access this resource' });
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+
     static async validateSuperAdminToken(req: Request, res: Response, next: NextFunction) {
         await AuthMiddleware.validateTokenWithRole(req, res, next, ['SUPER_ADMIN_ROLE']);
     }
