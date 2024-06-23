774e3d51826dbe042a3dfdb3c0fce0666bfcb204
Daniel Cobos <daniel.eduardo.cobos@hotmail.com>
2024-06-23 17:11:48 -0500

    23/06/2024: Adding isVisibleToUser field

--- src/domain/dtos/station/create-station.dto.ts
+++ src/domain/dtos/station/create-station.dto.ts
@@ -10,11 +10,12 @@ export class CreateStationDto {
         public readonly countryCode: string,
         public readonly coordinates: Coordinates,
         public readonly networkId: string,
+        public readonly isVisibleToUser: boolean,
         public readonly city?: string,        
     ) {}
 
     public static create(object: { [key: string]: any }): [ string?, CreateStationDto? ] {
-        const { name, state, countryCode, coordinates, networkId, city } = object;
+        const { name, state, countryCode, coordinates, networkId, isVisibleToUser, city } = object;
 
         if (!name) return ['Property name is required'];
         if (!state) return ['Property state is required']; // TODO: check if state is a valid state name
@@ -34,12 +35,28 @@ export class CreateStationDto {
         if (typeof latitude !== 'number') return ['Property coordinates.latitude is missing or not a number'];
 
         if(!networkId) return ['Property networkId is required'];
-
+        
+        if (!isVisibleToUser) return ['Property isVisibleToUser is required & must be a boolean'];
+        let isVisibleToUserBool: boolean;
+        if (typeof isVisibleToUser === 'boolean') {
+            isVisibleToUserBool = isVisibleToUser;
+        } else if (typeof isVisibleToUser === 'string') {
+            if (isVisibleToUser.toLowerCase() === 'true') {
+                isVisibleToUserBool = true;
+            } else if (isVisibleToUser.toLowerCase() === 'false') {
+                isVisibleToUserBool = false;
+            } else {
+                return ['Property isVisibleToUser must be a boolean or a string that represents a boolean'];
+            }
+        } else {
+            return ['Property isVisibleToUser must be a boolean'];
+        }
+        
         if (city) { // TODO: check if city is a valid state city
             if (typeof city !== 'string') return ['city property must be a string'];
             if (city.trim() === '') return ['city property must be a non-empty string'];
         };
 
-        return [undefined, new CreateStationDto( name, state, countryCode.toUpperCase(), coordinates, networkId, city )];
+        return [undefined, new CreateStationDto( name, state, countryCode.toUpperCase(), coordinates, networkId, isVisibleToUserBool, city )];
     }
 }
