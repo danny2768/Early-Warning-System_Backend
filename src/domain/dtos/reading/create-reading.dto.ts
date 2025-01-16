7917293343de8b29e54227583bf3bddcf0dbb22a
danny2768 <daniel.eduardo.cobos@hotmail.com>
2025-01-15 21:45:25 -0500

    Feat: receive a number instead of a string

--- src/domain/dtos/reading/create-reading.dto.ts
+++ src/domain/dtos/reading/create-reading.dto.ts
@@ -1,17 +1,18 @@
-
-
 export class CreateReadingDto {
 
     private constructor(
-        public readonly value: string,
+        public readonly value: number,
         public readonly sensor: string,        
     ) {}
 
     public static create(object: { [key: string]: any }): [ string?, CreateReadingDto? ] {
         const { value, sensor } = object;
         
-        if (!value) return ['Property value is required'];
+        if (value === undefined || value === null) return ['Property value is required'];
+        if (typeof value !== 'number') return ['Property value must be a number'];
+
         if (!sensor) return ['Property sensor is required'];                
+        if (typeof sensor !== 'string') return ['Property sensor must be a string'];
 
         return [undefined, new CreateReadingDto( value, sensor )];
     }
